63c1f603e228fe8a2ed4ea49086c98034d6c8575
Alex Nyago <nyagoa@stanbic.com>
2022-03-24 20:05:02 +0300

    big fixes

--- src/components/PreviewItinerary/Pricing.vue
+++ src/components/PreviewItinerary/Pricing.vue
@@ -1,16 +1,22 @@
 <!-- @format -->
 
 <template>
-    <div class="pricing text-right">
-        <h5 class="m-3 float-left">Pricing Summary</h5>
-        <b-table
+    <div class="pricing justify-content-center">
+        <h5 class="m-3 block">Pricing Details</h5><br>
+        <div class="card justify-content-center" style="width: 70%;">
+            <div class="card-body text-center ">
+                <div class="card-titled d-flex justify-content-center"> <h1><strong>{{ itinerary.total_cost | moneyFormat }}</strong></h1></div>
+                <p class="card-text">Total Price</p>
+            </div>
+        </div>
+        <!-- <b-table
             class="text-left"
             striped
             hover
-            :items="itinerary.summary"
+            :items="[{ type: 'to', cost: 1800 }]"
             table-variant="light"
-        ></b-table>
-        <strong>Total Price: {{ itinerary.total_cost | moneyFormat }}</strong>
+        ></b-table> -->
+        <!-- <strong>Total Price: {{ itinerary.total_cost | moneyFormat }}</strong>{{itinerary.summary}} -->
     </div>
 </template>
 
